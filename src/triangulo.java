c5d3760175a827e038faa5d5f8e9426363ee9e29
WendyOrtiz <Wortizm2@est.ups.edu.ec>
2024-06-16 16:00:22 -0500

    Add(number_of_commit): commit message

--- src/triangulo.java
+++ src/triangulo.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class triangulo {
+public class triangulo {
+    public static void main(String[] args) {
+        System.out.println("Triángulo de altura 3:");
+        printTriangle(3);
+        System.out.println("\nTriángulo de altura 5:");
+        printTriangle(5);
+        System.out.println("\nTriángulo de altura 7:");
+        printTriangle(7);
+    }
+    public static void printTriangle(int altura) {
+        for (int i = 1; i <= altura; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
 }
